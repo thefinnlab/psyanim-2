07f3b1b94e21e2d07629356c5644e760f1f53038
danrae pray <danrae.pray@outlook.com>
2023-06-13 17:33:33 -0500

    animation clip serialization is working and we have it unit-tested with jasmine!

---
+++ src/test/unit/animation_clip.spec.mjs
@@ -0,0 +1,57 @@
+import PsyanimAnimationClip from '../../utils/PsyanimAnimationClip.mjs';
+
+const fps = 60;
+const nSeconds = 8;
+
+const generateTestSamples = (clip, nSamples) => {
+
+    for (let j = 0; j < nSamples; ++j)
+    {
+        let sample = {
+            t: j,
+            x: j + 10,
+            y: j + 20,
+            rotation: j + 30,
+        };
+
+        clip.addSample(sample.t, { x: sample.x, y: sample.y }, sample.rotation);
+    }
+};
+
+const validateAnimationTestData = (clip) => {
+
+    let nSamples = clip.getSampleCount();
+
+    expect(nSamples).toBe(nSeconds * fps);
+
+    for  (let i = 0; i < nSamples; ++i)
+    {
+        let sample = clip.getSample(i);
+
+        expect(sample.t).toBe(i);
+        expect(sample.x).toBe(i + 10);
+        expect(sample.y).toBe(i + 20);
+        expect(sample.rotation).toBe(i + 30);
+    }
+};
+
+describe('serialization test', () => {
+    it('verify we can serialize / deserialize animation data w/o corruption', () => {
+
+        let fps = 60;
+        let nSeconds = 8;
+        let nSamples = nSeconds * fps;
+
+        let originalClip = new PsyanimAnimationClip();
+
+        generateTestSamples(originalClip, nSamples);
+
+        validateAnimationTestData(originalClip);
+
+        let serializedClipData = originalClip.toBuffer();
+
+        let clipCopy = PsyanimAnimationClip.fromBuffer(serializedClipData.buffer);
+
+        validateAnimationTestData(clipCopy);
+    });
+});
--- src/utils/PsyanimAnimationClip.mjs
+++ src/utils/PsyanimAnimationClip.mjs
@@ -1,5 +1,8 @@
 export default class PsyanimAnimationClip {
 
+    // sampleLength is number of floats necessary to represent a sample
+    static SAMPLE_LENGTH = 4;
+
     constructor() {
 
         this._transformData = [];
@@ -11,16 +14,18 @@ export default class PsyanimAnimationClip {
 
         let clip = new PsyanimAnimationClip();
 
+        let nSamples = data.length / PsyanimAnimationClip.SAMPLE_LENGTH;
+
         for (let i = 0; i < nSamples; ++i)
         {
             clip._transformData.push({
 
-                t: data[nSamples * i],
+                t: data[PsyanimAnimationClip.SAMPLE_LENGTH * i],
 
-                x: data[nSamples * i + 1], 
-                y: data[nSamples * i + 2],
+                x: data[PsyanimAnimationClip.SAMPLE_LENGTH * i + 1], 
+                y: data[PsyanimAnimationClip.SAMPLE_LENGTH * i + 2],
 
-                rotation: data[nSamples[i] + 3]
+                rotation: data[PsyanimAnimationClip.SAMPLE_LENGTH * i + 3]
             });
         }
 
@@ -29,27 +34,26 @@ export default class PsyanimAnimationClip {
 
     toFloat32Array() {
 
-        // sampleLength is number of floats necessary to represent a sample
-        const sampleLength = 3;
-
         let nSamples = this._transformData.length;
-        let data = new Float32Array(nSamples * sampleLength);
+        let data = new Float32Array(nSamples * PsyanimAnimationClip.SAMPLE_LENGTH);
 
         for (let i = 0; i < nSamples; ++i)
         {
             let transform = this._transformData[i];
 
             // we store data in this order: t, x, y, angle
-            data[sampleLength * i] = transform.t;
-            data[sampleLength * i + 1] = transform.pos.x;
-            data[sampleLength * i + 2] = transform.pos.y;
-            data[sampleLength * i + 3] = transform.rot;
+            data[PsyanimAnimationClip.SAMPLE_LENGTH * i] = transform.t;
+            data[PsyanimAnimationClip.SAMPLE_LENGTH * i + 1] = transform.x;
+            data[PsyanimAnimationClip.SAMPLE_LENGTH * i + 2] = transform.y;
+            data[PsyanimAnimationClip.SAMPLE_LENGTH * i + 3] = transform.rotation;
         }
+
+        return data;
     }
 
     toBuffer() {
 
-        return Buffer.from(this.toFloat32Array().Buffer);
+        return Buffer.from(this.toFloat32Array().buffer);
     }
 
     addSample(time, position, rotation) {
