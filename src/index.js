ab3de16a69ebf324d692f6a5dea9fcb133e15efa
danrae pray <danrae.pray@outlook.com>
2023-07-27 17:55:22 -0500

    exported jsPsych plugin to test with

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import PsyanimConstants from './core/PsyanimConstants';
 import PsyanimScene from './core/scene/PsyanimScene';
 import PsyanimComponent from './core/PsyanimConstants';
 
+import PsyanimJsPsychPlugin from './integrations/PsyanimJsPsychPlugin';
+
 export {
     PsyanimApp,
     PsyanimConstants,
     PsyanimScene,
-    PsyanimComponent
+    PsyanimComponent,
+
+    PsyanimJsPsychPlugin
 };
