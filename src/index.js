116d4d7f1cce7071852b23998b187336514773f6
danrae pray <danrae.pray@outlook.com>
2023-07-27 17:44:39 -0500

    attempting to clean up our module imports when using psyanim2 via npm

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import PsyanimApp from './core/PsyanimApp';
+import PsyanimConstants from './core/PsyanimConstants';
+import PsyanimScene from './core/scene/PsyanimScene';
+import PsyanimComponent from './core/PsyanimConstants';
+
+module.exports = {
+    PsyanimApp,
+    PsyanimConstants,
+    PsyanimScene,
+    PsyanimComponent
+};
